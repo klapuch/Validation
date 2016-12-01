55e7ee2a0a9f816016811ea099e5e4ab23fee6c3
Dominik Klapuch <klapuchdominik@gmail.com>
2016-12-01 22:32:54 +0100

    Fixed CS

--- Core/RangeRule.php
+++ Core/RangeRule.php
@@ -6,35 +6,35 @@ namespace Klapuch\Validation;
  * Rule for satisfying subject in allowed range
  */
 final class RangeRule implements Rule {
-	private $from;
-	private $to;
+	private $min;
+	private $max;
 
 	/**
-	 * @param int|string $from
-	 * @param int|string $to
+	 * @param int|string $min
+	 * @param int|string $max
 	 */
-	public function __construct($from, $to) {
-		$this->from = $from;
-		$this->to = $to;
+	public function __construct($min, $max) {
+		$this->min = $min;
+		$this->max = $max;
 	}
 
 	public function satisfied($subject): bool {
-		return in_array($subject, range($this->from(), $this->to()), true);
+		return in_array($subject, range($this->min(), $this->max()), true);
 	}
 
 	/**
 	 * The real top border from the range
 	 * @return int|string
 	 */
-	private function from() {
-		return max($this->from, $this->to);
+	private function min() {
+		return max($this->min, $this->max);
 	}
 
 	/**
 	 * The real bottom border from the range
 	 * @return int|string
 	 */
-	private function to() {
-		return min($this->from, $this->to);
+	private function max() {
+		return min($this->min, $this->max);
 	}
 }
